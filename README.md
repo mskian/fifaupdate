12aa2e71f25c130094202f3e599fe17248ad6000
mskian <veerfun23@gmail.com>
2018-06-26 20:51:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# FIFA 2018 Scores CLI
+
+Code Inspired from https://github.com/johnbfox/world-cup-scores-cli (I clone and Modified this for Learning Purpose)
+
+## Installing
+
+```sh
+git clone https://github.com/mskian/fifaupdate
+```
+
+## Install NPM
+
+```sh
+npm install
+```
+
+##Link NPM and Bash
+
+```sh
+sudo npm link
+```
+
+##Runnling
+
+```sh
+fifascore
+```
+
+```sh
+FIFA 2018 World Cup Score - Tuesday Jun 26th
+
+--------------------------------------
+
+AUS 0 - PER 1 (40') :: 7:30 PM
+ 
+GOALS:
+
+ 18' - PER : Andre CARRILLO
+
+--------------------------------------
+
+DEN 0 - FRA 0 (41') :: 7:30 PM
+
+--------------------------------------
+
+ISL 0 - CRO 0  :: 11:30 PM
+
+--------------------------------------
+
+NGA 0 - ARG 0  :: 11:30 PM
+
+--------------------------------------
+```
+
+*** Credits to estiens for the killer api
+https://github.com/estiens/world_cup_json
---
+++ index.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+var request = require('request')
+var moment  = require('moment')
+
+const url = 'http://worldcup.sfg.io/matches/today'
+
+request(url, (err, res, body)  => {
+  const matches = JSON.parse(body)
+  console.log('')
+  console.log('FIFA 2018 World Cup Score -', moment().format('dddd MMM Do'))
+  console.log('')
+  matches.map(match => {
+      const time = timeMap(match.status, match.time)
+      const goals = getGoals(match);
+      console.log('--------------------------------------')
+      console.log('')
+      console.log(`${match.home_team.code} ${match.home_team.goals} - ${match.away_team.code} ${match.away_team.goals} ${time} :: ${moment(match.datetime).format('LT')}`)
+      if(goals.length > 0){
+        console.log(' ')
+        console.log('GOALS:\n')
+        goals.map(goal => {
+          let goalString = ` ${goal.time} - ${goal.team} : ${goal.player}`;
+          if (goal.penalty){
+            goalString = goalString + ' (PEN)';
+          }else if(goal.ownGoal){
+            goalString = goalString + ' (OG)'
+          }
+          console.log(goalString)
+        })
+      }
+
+      console.log('')
+  });
+  console.log('--------------------------------------')
+});
+
+function timeMap(status, time){
+  if(status === 'in progress'){
+    return `(${time})`
+  }else if(status === 'completed'){
+    return '(FT)'
+  }else {
+    return ''
+  }
+}
+
+function getGoals(match){
+  const homeGoals = match.home_team_events.filter( event => event.type_of_event.includes('goal'))
+                         .map(event => { return {
+                           time: event.time,
+                           team: match.home_team.code,
+                           player: event.player,
+                           penalty: (event.type_of_event === 'goal-penalty'),
+                           ownGoal: (event.type_of_event === 'goal-own')
+                         } } )
+
+  const awayGoals = match.away_team_events.filter(event => event.type_of_event.includes('goal'))
+                         .map(event => { return {
+                           time: event.time,
+                           team: match.away_team.code,
+                           player: event.player,
+                           penalty: (event.type_of_event === 'goal-penalty'),
+                           ownGoal: (event.type_of_event === 'goal-own')
+                          } } )
+
+  return homeGoals.concat(awayGoals).sort( (a, b) => {
+    return parseInt(a.time.replace("\'", "")) - parseInt(b.time.replace("\'", ""))
+  });
+}
